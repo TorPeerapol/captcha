247f9389a97cd4f17272fc8219a1d34633e91b85
Peerapol Thichaiwong <15035t@gmail.com>
2016-10-12 14:54:14 +0700

    Create pattern2

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -2,17 +2,20 @@ function Captcha(){
 	var operator = ['+','-'];
 	var charOperand = ['ZERO','ONE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT','NINE'];
 	var intOperand = ['0','1','2','3','4','5','6','7','8','9'];
-	var result = '';
 
 	this.pattern1 = function(o,l,r){
 		return intOperand[l]+" "+operator[o-1]+" "+charOperand[r];
+	}
+
+	this.pattern2 = function(o,l,r){
+		return charOperand[l]+" "+operator[o-1]+" "+intOperand[r];
 	}
 
 	this.echo = function(p,o,l,r){
 		if(p === 1){
 			return this.pattern1(o,l,r);
 		}else if(p === 2){
-
+			return this.pattern2(o,l,r);
 		}
 	}
 }
@@ -20,5 +23,8 @@ describe("Captcha()",function() {
 	var captcha = new Captcha();
 	it('shold "1 + ONE" ',function(){
 		expect(captcha.echo(1,1,1,1)).toEqual("1 + ONE");
+	});
+	it('shold "ONE - 1" ',function(){
+		expect(captcha.echo(2,2,1,1)).toEqual("ONE - 1");
 	});
 });
