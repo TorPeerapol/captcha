64c5d00575d4fc7cf4f88e0a42c5c215a335bed6
Peerapol Thichaiwong <15035t@gmail.com>
2016-10-12 14:52:16 +0700

    Edit pattern1

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -4,17 +4,21 @@ function Captcha(){
 	var intOperand = ['0','1','2','3','4','5','6','7','8','9'];
 	var result = '';
 
-	this.echo = function(p,o,l,r){
-		return "ONE + 1";
+	this.pattern1 = function(o,l,r){
+		return intOperand[l]+" "+operator[o-1]+" "+charOperand[r];
 	}
 
-	this.pattern1 = function(o,l,r){
+	this.echo = function(p,o,l,r){
+		if(p === 1){
+			return this.pattern1(o,l,r);
+		}else if(p === 2){
 
+		}
 	}
 }
 describe("Captcha()",function() {
 	var captcha = new Captcha();
-	it('shold "ONE + 1" ',function(){
-		expect(captcha.echo(2,1,1,1)).toEqual("ONE + 1");
+	it('shold "1 + ONE" ',function(){
+		expect(captcha.echo(1,1,1,1)).toEqual("1 + ONE");
 	});
 });
