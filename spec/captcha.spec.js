ec0290e4f8b02d9d7c7e94e2dd9c99479c86b015
Peerapol Thichaiwong <15035t@gmail.com>
2016-10-12 14:42:19 +0700

    Create variable array

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,5 +1,9 @@
 function Captcha(){
-	this.echo = function(pattern,operator,leftOperand,rightOperand){
+	var operator = ['+','-'];
+	var charOperand = ['ZERO','ONE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT','NINE'];
+	var intOperand = [0,1,2,3,4,5,6,7,8,9];
+	
+	this.echo = function(p,o,l,r){
 		return "ONE + 1";
 	}
 }
